6fb7a8a1521e60a7e99915e9c633bceb8f09dd12
Manuel Salvador <manu.sacr@hotmail.com>
2023-07-12 17:38:10 -0300

    feat: added vuetify

--- src/App.vue
+++ src/App.vue
@@ -14,6 +14,7 @@ import HelloWorld from './components/HelloWorld.vue'
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
       </nav>
+      <v-btn> Mi boton de vuetify </v-btn>
     </div>
   </header>
 
