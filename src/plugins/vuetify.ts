0449069d96ad2ff150fa18546cc6a56499aaf4e9
Manuel Salvador <manu.sacr@hotmail.com>
2023-07-13 13:32:37 -0300

    fix(project): :art: added and configured @mdi/font

---
+++ src/plugins/vuetify.ts
@@ -0,0 +1,17 @@
+import '@mdi/font/css/materialdesignicons.css' // Ensure you are using css-loader
+import 'vuetify/styles'
+
+import { createVuetify } from 'vuetify'
+
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+  icons: {
+    defaultSet: 'mdi'
+  }
+})
+
+export default vuetify
