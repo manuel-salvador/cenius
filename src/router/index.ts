90b50a8d3ee9ffeaf183b7647b1947ec1426c1e4
Manuel Salvador <manu.sacr@hotmail.com>
2023-08-03 20:32:42 -0300

    feat(page): :sparkles: created products page

--- src/router/index.ts
+++ src/router/index.ts
@@ -24,6 +24,11 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/DashboardView.vue')
+    },
+    {
+      path: '/products',
+      name: 'products',
+      component: () => import('@/views/ProductsView.vue')
     }
   ]
 })
